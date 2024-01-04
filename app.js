a6edcecdcc436cc1eff9d931879b73123899b173
Hetal Nathvani <nathvanihetal@gmail.com>
2024-01-04 09:07:11 +0530

    Sample data and the data retrival api changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/sample.json`));
 
 // Routes
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({ status: "Success", data: tours });
+});
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from server side" });
 });
 
 app.post("/", (req, res) => {
+  console.log(tours);
   res.status(200).json({ message: "You can post on this..." });
 });
 
